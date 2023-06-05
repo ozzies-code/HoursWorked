4a6223b4570cb219be6773e2c1fa79729496f3db
Oswaldo Jesús Marín Pagés <83247451+ozzies-code@users.noreply.github.com>
2023-06-05 15:44:50 -0400

    Uploading Program Files in C: Hours Worked
    Uploading Program Files in C that calculates Hours Worked.

---
+++ PrecioHora2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  float horas, precioHora, salario;
+   char nombre[81];
+   char masDatos;
+   
+   masDatos == 'S';
+
+    puts("\tCalcula Salario");
+    puts("Introducir Horas, precio hora y nombre. \n");
+
+    while (masDatos == 'S' || masDatos == 's')
+    {
+        printf("Nombre: ");
+        gets(nombre);
+        
+    }
+  system("PAUSE");	
+  return 0;
+}
